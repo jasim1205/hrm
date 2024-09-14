e285c801e7d10234f47f4cc3604f1a8b7279b628
MD.Josim uddin <jasimuddinm180@gmail.com>
2024-09-14 22:33:43 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthenticationController as auth;
+use App\Http\Controllers\UserController as user;
+use App\Http\Controllers\RoleController as role;
+use App\Http\Controllers\DashboardController as dashboard;
+use App\Http\Controllers\PermissionController as permission;
+
+Route::get('/register', [auth::class, 'signUpForm'])->name('register');
+Route::post('/register', [auth::class, 'signUpStore'])->name('register.store');
+Route::get('/', [auth::class, 'signInForm'])->name('login');
+Route::post('/login', [auth::class, 'signInCheck'])->name('login.check');
+Route::get('/logout', [auth::class, 'signOut'])->name('logOut');
+
+Route::middleware(['checkauth'])->prefix('admin')->group(function () {
+    Route::get('dashboard', [dashboard::class, 'index'])->name('dashboard');
+});
+// Route::get('/', function () {
+//     return view('welcome');
+// });

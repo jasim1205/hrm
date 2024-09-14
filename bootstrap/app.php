9ef288ff030fbe762bc77a3d7f3bfd572f6ccebb
MD.Josim uddin <jasimuddinm180@gmail.com>
2024-09-14 23:58:30 +0600

    auth

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,9 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'checkauth' => \App\Http\Middleware\CheckAuth::class,
+            'checkrole' => \App\Http\Middleware\CheckRole::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
-
